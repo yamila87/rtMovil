bce12b9d230ad22968714f7f59825ba3d913538e
ysalomon <yamila.salomon@enerminds.com>
2015-09-07 11:39:00 -0300

    zzzzz

--- www/librt.web.js
+++ www/librt.web.js
@@ -294,7 +294,7 @@ showMsg= function (s) {
 }
 
 
-var appHost= (enAppMovil && CFGLIB.appUrl) ? CFGLIB.appUrl.replace(/[^\/]+$/,"") : 'https://10.70.251.55:8444/app/';
+var appHost= (enAppMovil && CFGLIB.appUrl) ? CFGLIB.appUrl.replace(/[^\/]+$/,"") : 'https://10.70.251.64:8444/app/';
 //var appHost='https://192.168.10.4:8443/app/';
 var host= location.host ? "//"+location.host+"/app/" : appHost; //XXX: hack for phonegap, move inside libRequire, implement differently in each runtime
 DBG.run && alert("HOST '"+host+"'");
